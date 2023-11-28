4c4eb2857b90548b1d1079231f73c372e9c524b2
Marlon <marlongarciab@hotmail.com>
2023-11-28 18:08:28 +0100

    Login, Signup, Post, Get, Change + delete now works

--- functions/deleteNote/index.js
+++ functions/deleteNote/index.js
@@ -1,47 +1,39 @@
-const {sendResponse} = require('../../responses/index')
+const { sendResponse } = require('../../responses/index');
 const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
 const middy = require('@middy/core');
 const { validateToken } = require('../middleware/auth');
 
-
-const deleteNote = async(event,context)=>{
-   
-    if(event?.error && event?.error === '401')
-    return sendResponse(401, {success: false, message: 'invalid token'});
+const deleteNote = async (event, context) => {
+    if (event?.error && event?.error === '401') {
+        return sendResponse(401, { success: false, message: 'invalid token' });
+    }
 
     const requestBody = JSON.parse(event.body);
-    const {id} = requestBody
+    const { id, username } = requestBody; 
 
-        try{
+    try {
+        const { Item: noteToDelete } = await db.get({
+            TableName: 'notes-db',
+            Key: { id }
+        }).promise();
 
-        const {Items} = await db.scan({
-            TableName: 'notes-db'
-        }).promise()
-
-        const deleteNote = Items.find((note) => note.id === id);
-
-        if (!deleteNote) {
+        if (!noteToDelete) {
             return sendResponse(404, { success: false, message: "Cannot find note with this id" });
         }
 
-        if (event.username != noteToDelete.username){
-            return sendResponse(404, { success: false, 
-                message: "You can not kill what you did\'nt create" });
-  
-
+        if (username !== noteToDelete.username) {
+            return sendResponse(403, { success: false, message: "You do not have permission to delete this note" });
         }
+
         const date = new Date().toISOString();
-
-        const modifiedAt = `${date}`
-
+        const modifiedAt = `${date}`;
 
         await db.update({
             TableName: 'notes-db',
-            Key: { id: noteDelete.id },
+            Key: { id },
             ReturnValues: 'ALL_NEW',
             UpdateExpression: "SET #isActive = :isActive, #modifiedAt = :modifiedAt",
-
             ExpressionAttributeValues: {
                 ":isActive": false,
                 ":modifiedAt": modifiedAt
@@ -54,20 +46,15 @@ const deleteNote = async(event,context)=>{
 
         await db.delete({
             TableName: 'notes-db',
-            Key:    { id: noteDelete.id}
+            Key: { id }
         }).promise();
 
-
-        return sendResponse(200, {message: "The note is deleted id: " + noteDelete.id })
-    } catch(error){
-
-        return sendResponse(500, {success: false, message: "could not delete"})
-
+        return sendResponse(200, { message: "The note is deleted id: " + id });
+    } catch (error) {
+        return sendResponse(500, { success: false, message: "could not delete" });
     }
-}
+};
 
+const handler = middy(deleteNote).use(validateToken);
 
-const handler = middy(deleteNote)
-.use(validateToken)
-
-module.exports = {handler};
+module.exports = { handler };
